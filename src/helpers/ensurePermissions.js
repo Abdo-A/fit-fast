6472cd1903f46ec3baa86dcbd471db1ea5cf351b
Abdo <abdopoetry@gmail.com>
2019-08-10 17:20:06 +0200

    Improve ensurePermissions functions

--- src/helpers/ensurePermissions.js
+++ src/helpers/ensurePermissions.js
@@ -5,11 +5,10 @@ export const ensureCameraPermission = async (callback) => {
   const { status: cameraStatus } = await Permissions.getAsync(
     Permissions.CAMERA
   );
-  if (cameraStatus !== 'granted') {
-    await Permissions.askAsync(Permissions.CAMERA);
-  }
-
-  if (cameraStatus === 'granted') {
+  if (cameraRollStatus !== 'granted') {
+    await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (callback) callback();
+  } else if (cameraRollStatus === 'granted') {
     if (callback) callback();
   }
 };
@@ -21,9 +20,8 @@ export const ensureCameraRollPermission = async (callback) => {
   );
   if (cameraRollStatus !== 'granted') {
     await Permissions.askAsync(Permissions.CAMERA_ROLL);
-  }
-
-  if (cameraRollStatus === 'granted') {
+    if (callback) callback();
+  } else if (cameraRollStatus === 'granted') {
     if (callback) callback();
   }
 };
